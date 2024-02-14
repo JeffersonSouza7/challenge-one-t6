1a19e8620b47112cdf64f663cd56eb79bb0b93a7
JeffersonSouza7 <158853940+JeffersonSouza7@users.noreply.github.com>
2024-02-14 11:35:24 -0300

    Update app.js

--- Challenge One/assets/js/app.js
+++ Challenge One/assets/js/app.js
@@ -2,25 +2,37 @@
 // Função para criptografar o texto usando a cifra específica
 function criptografarTexto() {
     var texto = document.getElementById('texto_Input').value;
-    var textoCriptografado = texto.replace(/e/g, 'enter')
-                                   .replace(/i/g, 'imes')
-                                   .replace(/a/g, 'ai')
-                                   .replace(/o/g, 'ober')
-                                   .replace(/u/g, 'ufat');
+    
+    // Verifica se o texto contém apenas letras minúsculas e sem acento
+    if (/^[a-z]+$/.test(texto)) {
+        var textoCriptografado = texto.replace(/e/g, 'enter')
+                                       .replace(/i/g, 'imes')
+                                       .replace(/a/g, 'ai')
+                                       .replace(/o/g, 'ober')
+                                       .replace(/u/g, 'ufat');
 
-    document.getElementById('texto_Output').innerHTML = '<p>' + textoCriptografado + '</p>';
+        document.getElementById('texto_Output').innerHTML = '<p>' + textoCriptografado + '</p>';
+    } else {
+        alert('Por favor, insira apenas letras minúsculas e sem acento.');
+    }
 }
 
 // Função para descriptografar o texto usando a cifra específica
 function descriptografarTexto() {
     var texto = document.getElementById('texto_Input').value;
-    var textoDescriptografado = texto.replace(/enter/g, 'e')
-                                     .replace(/imes/g, 'i')
-                                     .replace(/ai/g, 'a')
-                                     .replace(/ober/g, 'o')
-                                     .replace(/ufat/g, 'u');
+    
+    // Verifica se o texto contém apenas letras minúsculas e sem acento
+    if (/^[a-z]+$/.test(texto)) {
+        var textoDescriptografado = texto.replace(/enter/g, 'e')
+                                         .replace(/imes/g, 'i')
+                                         .replace(/ai/g, 'a')
+                                         .replace(/ober/g, 'o')
+                                         .replace(/ufat/g, 'u');
 
-    document.getElementById('texto_Output').innerHTML = '<p>' + textoDescriptografado + '</p>';
+        document.getElementById('texto_Output').innerHTML = '<p>' + textoDescriptografado + '</p>';
+    } else {
+        alert('Por favor, insira apenas letras minúsculas e sem acento.');
+    }
 }
 
 // Função para copiar o texto criptografado/descriptografado para a área de transferência
