3df8193efd670da17305a209f45a30aa6644f45d
JeffersonSouza7 <158853940+JeffersonSouza7@users.noreply.github.com>
2024-02-14 09:53:51 -0300

    Add files via upload

---
+++ Challenge One/assets/js/app.js
@@ -0,0 +1,34 @@
+
+// Função para criptografar o texto usando a cifra específica
+function criptografarTexto() {
+    var texto = document.getElementById('texto_Input').value;
+    var textoCriptografado = texto.replace(/e/g, 'enter')
+                                   .replace(/i/g, 'imes')
+                                   .replace(/a/g, 'ai')
+                                   .replace(/o/g, 'ober')
+                                   .replace(/u/g, 'ufat');
+
+    document.getElementById('texto_Output').innerHTML = '<p>' + textoCriptografado + '</p>';
+}
+
+// Função para descriptografar o texto usando a cifra específica
+function descriptografarTexto() {
+    var texto = document.getElementById('texto_Input').value;
+    var textoDescriptografado = texto.replace(/enter/g, 'e')
+                                     .replace(/imes/g, 'i')
+                                     .replace(/ai/g, 'a')
+                                     .replace(/ober/g, 'o')
+                                     .replace(/ufat/g, 'u');
+
+    document.getElementById('texto_Output').innerHTML = '<p>' + textoDescriptografado + '</p>';
+}
+
+// Função para copiar o texto criptografado/descriptografado para a área de transferência
+function copiarTexto() {
+    var texto = document.getElementById('texto_Output').innerText;
+    navigator.clipboard.writeText(texto).then(function() {
+        alert('Texto copiado com sucesso!');
+    }, function(err) {
+        console.error('Erro ao copiar o texto: ', err);
+    });
+}
